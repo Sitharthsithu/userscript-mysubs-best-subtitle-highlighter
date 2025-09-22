95a988cd2a8ecf718ede358494d12ba0b406681f
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-22 20:20:07 +0200

    Fix license

--- mysubs-best-subtitle-highlighter.user.js
+++ mysubs-best-subtitle-highlighter.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         MySubs Best Subtitle Highlighter
 // @namespace    https://greasyfork.org/users/1002054-igorskyflyer
-// @version      v1.0.4
+// @version      v1.0.5
 // @description  Highlights the most-voted subtitle per language on MySubs film pages. Works only for films!
 // @author       igorskyflyer
-// @date         2025-09-16
+// @date         2025-09-22
 // @match        https://my-subs.co/film*
 // @match        https://my-subs.com/film*
 // @match        https://my-subs.net/film*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=my-subs.co
 // @run-at       document-end
 // @grant        none
-// @license      GPL3
+// @license      GPL-3.0-or-later
 // @compatible   chrome
 // @compatible   firefox
 // @compatible   opera
